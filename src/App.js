d519cbbbaa1820eface87dc195e81138e60fb93c
RasuwanK <kalharaweragala@gmail.com>
2022-04-04 13:07:10 +0530

    Hello world

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Label from './Label';
 
 import './styles/app.css';
 
+// Hello this is a comment
+
 const contacts = [
     {first:'Rasuwan',second:'Kalhara'},
     {first:'Nethula',second:'Dulsara'},
